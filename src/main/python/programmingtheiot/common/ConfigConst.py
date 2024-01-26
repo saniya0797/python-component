8cd1c1522cccaec46ef76664b1b7f0038844bfb7
saniya0797 <athani.s@northeastern.edu>
2024-01-25 16:19:10 -0800

    module2

--- src/main/python/programmingtheiot/common/ConfigConst.py
+++ src/main/python/programmingtheiot/common/ConfigConst.py
@@ -196,7 +196,8 @@ CDA_SYSTEM_PERF_MSG_RESOURCE          = PRODUCT_NAME + '/' + CONSTRAINED_DEVICE 
 # the directory structure for python-components
 
 # NOTE: You will need to update this!!
-DEFAULT_CONFIG_FILE_NAME = '../../../../../../../config/PiotConfig.props'
+
+DEFAULT_CONFIG_FILE_NAME = '/root/python-component/config/PiotConfig.props'
 DEFAULT_CRED_FILE_NAME   = '../../../../../../../cred/PiotCred.props'
 
 TEST_GDA_DATA_PATH_KEY = 'testGdaDataPath'
