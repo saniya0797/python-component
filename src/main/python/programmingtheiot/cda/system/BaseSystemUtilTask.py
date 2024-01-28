b57328bf4d00860d26418ed4b724d4dfbac91d5e
saniya0797 <69213333+saniya0797@users.noreply.github.com>
2024-01-28 00:26:14 -0800

    module2-modified

--- src/main/python/programmingtheiot/cda/system/BaseSystemUtilTask.py
+++ src/main/python/programmingtheiot/cda/system/BaseSystemUtilTask.py
@@ -9,22 +9,43 @@
 
 import programmingtheiot.common.ConfigConst as ConfigConst
 
-class BaseSystemUtilTask():
-	"""
-	Shell implementation representation of class for student implementation.
-	
-	"""
-	
-	def __init__(self, name: str = ConfigConst.NOT_SET, typeID: int = ConfigConst.DEFAULT_SENSOR_TYPE):
-		self.name = name
-		self.typeID = typeID
-	
-	def getName(self) -> str:
-		return self.name
-	
-	def getTypeID(self) -> int:
-		return self.typeID
-	
-	def getTelemetryValue(self) -> float:
-		pass
-	
+
+class BaseSystemUtilTask:
+    """
+    Shell implementation representation of class for student implementation.
+    """
+
+    def __init__(self, name: str = ConfigConst.NOT_SET, typeID: int = ConfigConst.DEFAULT_SENSOR_TYPE):
+        """
+        Constructor to initialize the BaseSystemUtilTask object.
+
+        :param name: The name of the system utilization task.
+        :param typeID: The type ID of the system utilization task.
+        """
+        self.name = name
+        self.typeID = typeID
+
+    def getName(self) -> str:
+        """
+        Gets the name of the system utilization task.
+
+        :return: The name of the task.
+        """
+        return self.name
+
+    def getTypeID(self) -> int:
+        """
+        Gets the type ID of the system utilization task.
+
+        :return: The type ID of the task.
+        """
+        return self.typeID
+
+    def getTelemetryValue(self) -> float:
+        """
+        Template method definition. Sub-class will implement this to retrieve
+        the system utilization measure.
+
+        :return: The telemetry value as a float.
+        """
+        pass
