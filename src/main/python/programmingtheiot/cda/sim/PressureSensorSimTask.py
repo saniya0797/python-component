d1f27d6c3a1a55ca340ddb791438b1b8dbed95ff
saniya0797 <athani.s@northeastern.edu>
2024-02-01 19:38:39 -0800

    labmodule3

--- src/main/python/programmingtheiot/cda/sim/PressureSensorSimTask.py
+++ src/main/python/programmingtheiot/cda/sim/PressureSensorSimTask.py
@@ -8,11 +8,10 @@
 #
 
 import logging
-
+import programmingtheiot.common.ConfigConst as ConfigConst
 from programmingtheiot.cda.sim.BaseSensorSimTask import BaseSensorSimTask
 from programmingtheiot.cda.sim.SensorDataGenerator import SensorDataGenerator
 
-from programmingtheiot.data.SensorData import SensorData
 
 class PressureSensorSimTask(BaseSensorSimTask):
 	"""
@@ -20,6 +19,12 @@ class PressureSensorSimTask(BaseSensorSimTask):
 	
 	"""
 
-	def __init__(self):
-		pass
-	
+	def __init__(self,dataSet = None):
+		super( \
+			PressureSensorSimTask, self).__init__( \
+				name = ConfigConst.PRESSURE_SENSOR_NAME, \
+				typeID = ConfigConst.PRESSURE_SENSOR_TYPE, \
+				dataSet = dataSet, \
+				minVal = SensorDataGenerator.LOW_NORMAL_ENV_PRESSURE, \
+				maxVal = SensorDataGenerator.HI_NORMAL_ENV_PRESSURE)
+		
